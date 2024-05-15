dda77acac1ad003bcd4b08f5f3a3579091259314
Mark <markus.anthony.garcia@gmail.com>
2024-05-15 17:19:44 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'jollof',
+  webDir: 'www'
+};
+
+export default config;
