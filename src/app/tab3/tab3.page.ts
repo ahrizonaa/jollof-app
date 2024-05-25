77225a4a7f0830cfa1f26f6e744f35f66edbaa8e
Mark <markus.anthony.garcia@gmail.com>
2024-05-24 22:23:56 -0500

    settings placeholders

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -5,16 +5,39 @@ import {
   IonTitle,
   IonContent,
   IonButton,
+  IonIcon,
+  IonList,
+  IonItem,
+  IonLabel,
 } from '@ionic/angular/standalone';
 import { UserService } from '../services/user.service';
 
+import { addIcons } from 'ionicons';
+import { airplane, wifi, bluetooth, call } from 'ionicons/icons';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton],
+  imports: [
+    IonIcon,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonButton,
+    IonList,
+    IonItem,
+    IonLabel,
+  ],
 })
 export class Tab3Page {
-  constructor(protected user: UserService) {}
+  constructor(protected user: UserService) {
+    addIcons({
+      airplane,
+      wifi,
+      bluetooth,
+      call,
+    });
+  }
 }
