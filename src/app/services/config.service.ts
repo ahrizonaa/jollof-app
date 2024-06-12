5e97661b0c331bd8933ed76e746aafd441267c7f
Mark <markus.anthony.garcia@gmail.com>
2024-06-12 01:40:27 -0500

    config

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import { ApiService } from './api.service';
+import { HttpClient } from '@angular/common/http';
+import { api } from '../utility/Api';
 
 @Injectable({
   providedIn: 'root',
@@ -9,10 +10,12 @@ export class ConfigService {
   googleClientId: BehaviorSubject<string> = new BehaviorSubject(null as any);
   facebookAppId: BehaviorSubject<string> = new BehaviorSubject(null as any);
 
-  constructor(private api: ApiService) {
-    this.api.get('appsettings').subscribe((res: any) => {
-      this.googleClientId.next(res.googleClientId);
-      this.facebookAppId.next(res.facebookAppId);
-    });
+  constructor(private http: HttpClient) {
+    this.http
+      .get(api.origin + '/core/config?appName=jollof')
+      .subscribe((res: any) => {
+        this.googleClientId.next(res.googleClientId);
+        this.facebookAppId.next(res.facebookAppId);
+      });
   }
 }
--- src/app/utility/Api.ts
+++ src/app/utility/Api.ts
@@ -1,16 +1,18 @@
 const env = {
-  dev: 'http://localhost:8080/jollof',
-  prod: 'https://charmee-webservices-7sgqd.ondigitalocean.app/jollof',
+  dev: 'http://localhost:8080',
+  prod: 'https://charmee-webservices-7sgqd.ondigitalocean.app',
 };
 
 export class Api {
+  origin: string;
   url: string;
 
   constructor() {
-    this.url =
+    this.origin =
       location.href.match(/(localhost|127\.0\.0\.1)/i) == null
         ? env.prod
         : env.dev;
+    this.url = this.origin + '/jollof';
   }
 }
 
